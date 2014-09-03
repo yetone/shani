c5b109c11150e93704d3f2efca6d72a5241722d0
yetone <i@yetone.net>
2014-09-03 17:53:16 +0800

    big refractor

--- shani.js
+++ shani.js
@@ -11,11 +11,8 @@
         var sf = (a === 'if') ? '' : ' } ';
         return sf + a + ' (' + b + ') { ';
       });
-      tpl = tpl.replace(/\{%\s*(for\s+.*?)\s*%\}(.*?)(\{%\s*end\s*%\})/g, function(_, exp, content, end) {
-        exp = exp.replace(/for\s+([a-zA-Z_][a-zA-Z0-9_]*),\s*([a-zA-Z_][a-zA-Z0-9_]*)\s+in\s+([a-zA-Z_][a-zA-Z0-9_]*)/, function(_, k, v, lst) {
-          return 'for (var ' + k + ' in ' + lst + ') { var ' + v + ' = ' + lst + '[' + k + ']; ';
-        });
-        return exp + content + end;
+      tpl = tpl.replace(/\{%\s*for\s+([a-zA-Z_][a-zA-Z0-9_]*),\s*([a-zA-Z_][a-zA-Z0-9_]*)\s+in\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*%\}/, function(_, k, v, lst) {
+        return 'for (var ' + k + ' in ' + lst + ') { var ' + v + ' = ' + lst + '[' + k + ']; ';
       });
       tpl = tpl.replace(/\{%\s*else\s*%\}/g, ' } else { ');
       tpl = tpl.replace(/\{%\s*end\s*%\}/g, ' } ');
